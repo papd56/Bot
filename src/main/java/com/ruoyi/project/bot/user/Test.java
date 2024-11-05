04b2b72ca121286d0be37832c8bd78378ef2e2e0
hawkins <bashen976@gmail.com>
2024-11-05 12:18:24 +0800

    uuid

---
+++ src/main/java/com/ruoyi/project/bot/user/Test.java
@@ -0,0 +1,17 @@
+package com.ruoyi.project.bot.user;
+
+import com.ruoyi.common.utils.uuid.UUID;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
